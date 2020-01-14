e5fdedfd67a8f9bad39db192f0af975a107a4360
Rachel van Grootheest <rachelvg12@gmail.com>
2020-01-14 16:45:57 +0100

    added part 2 to the file with all the parts

--- python_assignment.py
+++ python_assignment.py
@@ -6,6 +6,7 @@ with open('precipitation.json', encoding='utf8') as file:
 
 #select Seattle measurements
 seattle = 'GHCND:US1WAKG0038'
+sum_measurement = 0
 monthly_sum_measurement = [0]*12
 for measurement in data:
     if measurement ['station'] == seattle:
@@ -13,8 +14,24 @@ for measurement in data:
         # list sum of measurements per month 
         month = int(measurement_date[1])-1
         monthly_sum_measurement[month] += measurement['value']
+        # calculate sum of precipitation for whole year
+        sum_measurement += measurement['value']
+
+#relative precipitation  
+percentage_month = []
+for month in monthly_sum_measurement:
+    percentage = (month/sum_measurement) * 100
+    percentage_month.append(percentage)
+
 print(monthly_sum_measurement)
+print(sum_measurement)
+print(percentage_month)
 
 # save as json file 
 with open('python_assignment.json', 'w', encoding='utf8') as file:
     json.dump(monthly_sum_measurement, file)
+
+# save as json file 
+with open('python_assignment.json', 'w', encoding='utf8') as file:
+    json.dump(percentage_month, file)
+
